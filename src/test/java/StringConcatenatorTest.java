18d0a76a7f9c18786dc3ba469dc042843d96f4ea
sanyi <csisanyi@gmail.com>
2018-05-29 16:06:34 +0200

    test class corrected

--- src/test/java/StringConcatenatorTest.java
+++ src/test/java/StringConcatenatorTest.java
@@ -4,7 +4,7 @@ import org.junit.Test;
 public class StringConcatenatorTest {
 
     @Test
-    public String CconatTest(String s1, String s2) {
+    public void CconatTest() {
         Assert.assertEquals("Majom", StringConcatenator.concat("Ma","jom"));
     }
 }
